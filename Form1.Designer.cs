6a98dc54d832259809c74774c854fc50a18cf837
Ammon Johnson <ammonsawesome@gmail.com>
2021-07-12 14:04:14 -0700

    Got the double number function working

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -73,6 +73,7 @@ namespace GettingToKnowSourceControl
             this.Controls.Add(this.DoubleBtn);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,37 @@ namespace GettingToKnowSourceControl
             InitializeComponent();
         }
 
+        // buttonValue keeps track of the number that needs to be times by 2
+        long buttonValue = 1;
+
         private void DoubleBtn_Click(object sender, EventArgs e)
         {
+            // If buttonValue is greater than what a long can store stop the program
+            if(buttonValue * 2 < 922337203685477580)
+            {
+                buttonValue *= 2;
+                // Display the number when the form loads
+                DisplayNumber(buttonValue);
+            }
 
+            else
+            {
+                MessageBox.Show("Good job you reached the limit!", "Information"
+                                , MessageBoxButtons.OK
+                                , MessageBoxIcon.Information);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Display the number when the form loads
+            DisplayNumber(buttonValue);
+        }
+
+        // Display the number in the label
+        public void DisplayNumber(long buttonValue)
+        {
+            valueLabel.Text = String.Format("{0:n0}", buttonValue);
         }
     }
 }
