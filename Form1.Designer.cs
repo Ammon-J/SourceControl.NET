eb5318ad3c65e9895dce62db2bda13ccd7984b71
Ammon Johnson <ammonsawesome@gmail.com>
2021-07-12 14:14:52 -0700

    Added a reandom compliment generator

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,14 +32,17 @@ namespace GettingToKnowSourceControl
             this.DoubleBtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.valueLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.compLabel = new System.Windows.Forms.Label();
+            this.getCompBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // DoubleBtn
             // 
             this.DoubleBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DoubleBtn.Location = new System.Drawing.Point(78, 186);
+            this.DoubleBtn.Location = new System.Drawing.Point(105, 199);
             this.DoubleBtn.Name = "DoubleBtn";
-            this.DoubleBtn.Size = new System.Drawing.Size(245, 119);
+            this.DoubleBtn.Size = new System.Drawing.Size(195, 99);
             this.DoubleBtn.TabIndex = 0;
             this.DoubleBtn.Text = "Double Amount";
             this.DoubleBtn.UseVisualStyleBackColor = true;
@@ -58,20 +61,53 @@ namespace GettingToKnowSourceControl
             // valueLabel
             // 
             this.valueLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.valueLabel.Location = new System.Drawing.Point(78, 98);
+            this.valueLabel.Location = new System.Drawing.Point(95, 99);
             this.valueLabel.Name = "valueLabel";
-            this.valueLabel.Size = new System.Drawing.Size(245, 29);
+            this.valueLabel.Size = new System.Drawing.Size(216, 29);
             this.valueLabel.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(454, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(346, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Click The Button To Get a Compliment\r\n";
+            // 
+            // compLabel
+            // 
+            this.compLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.compLabel.Location = new System.Drawing.Point(519, 99);
+            this.compLabel.Name = "compLabel";
+            this.compLabel.Size = new System.Drawing.Size(216, 29);
+            this.compLabel.TabIndex = 4;
+            // 
+            // getCompBtn
+            // 
+            this.getCompBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getCompBtn.Location = new System.Drawing.Point(528, 200);
+            this.getCompBtn.Name = "getCompBtn";
+            this.getCompBtn.Size = new System.Drawing.Size(195, 99);
+            this.getCompBtn.TabIndex = 5;
+            this.getCompBtn.Text = "Get a Compliment";
+            this.getCompBtn.UseVisualStyleBackColor = true;
+            this.getCompBtn.Click += new System.EventHandler(this.getCompBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(833, 467);
+            this.ClientSize = new System.Drawing.Size(833, 395);
+            this.Controls.Add(this.getCompBtn);
+            this.Controls.Add(this.compLabel);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.valueLabel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.DoubleBtn);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -84,6 +120,9 @@ namespace GettingToKnowSourceControl
         private System.Windows.Forms.Button DoubleBtn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label valueLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label compLabel;
+        private System.Windows.Forms.Button getCompBtn;
     }
 }
 
